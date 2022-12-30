50a35613bf5efea7ecfb59702940c05d36c207d1
akina2333 <121144181+akina2333@users.noreply.github.com>
2022-12-30 16:25:10 +0800

    修改delete函数

--- 修改版.cpp
+++ 修改版.cpp
@@ -1,6 +1,8 @@
 #include <stdio.h>		
 #include <string.h>			
-#include <time.h>			
+#include <time.h>	
+#include <malloc.h>	
+#include <stdlib.h>	
 #define money  0.03  //停车场计费	 
 #define PMAX  5  //停车场容量 
 #define SMAX  4  //便道容量 
@@ -34,10 +36,6 @@ typedef struct{
 	int front, rear;		
 }Waiting; 
 
-typedef struct {
-    char* spots[PMAX];
-} ParkingLot;
-
 CarNode c;
 Park p;
 Sidewalk s;
@@ -47,7 +45,7 @@ Waiting w;
 char     C[10];  //待查询车牌号 
 
 void Arrive();	
-void Leave();		
+void CarLeave();		
 void Stop_To_Pave();	
 void Stop_To_Buff();		
 void LeaveSidewalk();		
@@ -57,9 +55,16 @@ void Welcome();
 void Welcome2();
 void CarLeave_menu();
 void Search();
-void Delete();
-void Change(); 
+int Delete();
 
+
+void Push(Park p,CarNode x){
+	if(p.top>=PMAX-1){
+		return;
+	}else{
+		p.Park[++p.top]=x;
+	}
+}
 
 void Arrive(){						 
 	printf("请输入您的车牌号：");
@@ -112,7 +117,6 @@ void Leave(){
 	printf("未查询到该车信息。\n"); 
 }
 
-
 void Search(){
 	printf("请输入要查询的车牌号：\n");
 	scanf("%s", &C);
@@ -124,7 +128,7 @@ void Search(){
 			if(strcmp(p.Park[i].number,C)==0){
 			printf("此汽车在停车场内，信息如下：\n");
 			printf("\t车牌号\t\t停车位序\t当前所需支付金额\t进入时间\t\n");
-			printf("\t%s\t第%d个\t\t%0.f元\t\t\t%s",p.Park[i].number,i+1,money*(t-p.Park[i].ArriveTime),p.Park[i].ct);
+			printf("\t%s\t\t第%d个\t\t%0.f元\t\t\t%s",p.Park[i].number,i+1,money*(t-p.Park[i].ArriveTime),p.Park[i].ct);
 			flag=1;
 			break;
 			} 
@@ -136,7 +140,7 @@ void Search(){
 			if(strcmp(s.Side[i].number,C)==0){
 				printf("此汽车停在便道上\n");
 				printf("\t车牌号\t\t停车位序\n");
-				printf("\t%s\t第%d个",s.Side[i].number, k);
+				printf("\t%s\t\t第%d个",s.Side[i].number, k);
 				flag=2;
 				break;
 			}
@@ -190,7 +194,6 @@ void LeaveSidewalk(){
 	}
 }
 
-
 void Stop_To_Buff(){
 	while (p.top >= 0){	
 		if(0 == strcmp(p.Park[p.top].number, C)){
@@ -238,7 +241,6 @@ void Stop_To_Pave(){
 	}
 }
 
-
 void DisPlay(){
 	int i = p.top;
 	if(i==-1)
@@ -247,12 +249,11 @@ void DisPlay(){
 	int t = time(&t1);
 	printf("\t车牌号\t\t停放时间\t当前所需支付金额\t停放位序\n");
 	while(i != -1){
-		printf("\t%s\t%d分%d秒\t\t%.0f元\t\t\t第%d个\n", p.Park[i].number, 
+		printf("\t%s\t\t%d分%d秒\t\t%.0f元\t\t\t第%d个\n", p.Park[i].number, 
 		(t - p.Park[i].ArriveTime)/60,(t - p.Park[i].ArriveTime)%60,money*(t - p.Park[i].ArriveTime), i+1);
 		i--;
 	}
 }
-
 
 void DisPlayPave(){
 	int i = s.front;
@@ -261,11 +262,10 @@ void DisPlayPave(){
 	printf("便道目前为空\n");
 	printf("\t车牌号\t\t停放位序\n");
 	while(i != s.rear && k <=s.length){  
-		printf("\t%s\t第%d个\n", s.Side[i].number, k++);
+		printf("\t%s\t\t第%d个\n", s.Side[i].number, k++);
 		i =(i+1)%SMAX;
 	}
 }
-
 
 void CarLeave_menu(){
 	while(1){
@@ -274,67 +274,68 @@ void CarLeave_menu(){
 		scanf("%d", &i);
 		if(1 == i)  Leave();
 		if(2 == i)  LeaveSidewalk();
-		if(3 == i)  return;
+		if(3 == i) 	return;
 		printf("\n返回请输入0\n");
-		
-		if(flag==0){
+		top:								// goto 标志位 
+			scanf("%d", &flag);
+		if(0 ==flag){
 			continue;
 		}
 		else{
 			printf("您的输入有误，请重新输入\n");
-		
+			goto top;						// goto 到指定标志位 top
 		}
 	}	
 } 
-void Delete(){
-printf("请输入要删除信息的车牌号：\n");
-	scanf("%s", &C);
-	int i,j,k,d,flag=0;        
-	time_t t1;
-	long int t = time(&t1);
+
+int Delete(){
+	printf("请输入要删除的车的车牌号：\n");
+	scanf("%s",&C);
+	int i,flag=0;       
 	if(p.top>= 0){
 		for(i =p.top;i>=0;i--){
 			if(strcmp(p.Park[i].number,C)==0){
-			printf("是否删除以下信息\n");
-			scanf("%d",&d);
-			printf("\t车牌号\t\t停车位序\t当前所需支付金额\t进入时间\t\n");
-			printf("\t%s\t第%d个\t\t%0.f元\t\t\t%s",p.Park[i].number,i+1,money*(t-p.Park[i].ArriveTime),p.Park[i].ct);
 			flag=1;
 			break;
-			if(d==1){
-			   for(int j=i;j>=0;j--)
-			   {
-			   	p.Park[j].number=p.Park[j+1].number;
-			   	strcpy(p.Park[j].number=p.Park[j+1].number);
-			   	money*(t-p.Park[i].ArriveTime)=money*(t-p.Park[i+1].ArriveTime);
-			   	p.Park[i].ct=p.Park[i+1].ct;
-		        }
-			   }
-			   
+			} 
 		}
 	}
-}	
+	if(flag==0){
+		printf("停车场内未查询到该汽车信息！\n");
+	}
+	if(flag==1){
+		Park s;
+		s.top=0;
+		while(p.top>-1){
+			if(strcmp(p.Park[p.top].number,C)==0){
+				p.top--;
+			}else{
+				s.top++;
+				s.Park[s.top+1]=p.Park[p.top];
+				p.top--;
+			}
+		}
+		while(s.top>0){
+			p.top++;
+			p.Park[p.top+1]=s.Park[s.top];
+			s.top--;
+		}
+		printf("删除成功！"); 
+	} 
+	return 1;
 }
 
-
-void change(ParkingLot* lot, int top, const char* new_plate)
-{
-    lot->spots[top] = new_plate;
-}
-ParkingLot lot;
-change(&lot,top,C);
-
-	
-void Welcome1(){
+void Welcome1(){     // 初始界面 
     printf ("\n\t-------------------------Welcome！---------------------\n");
     printf ("\t                                                      \n");
     printf ("\t                   1.进入用户系统                 \n\n");
     printf ("\t                   2.进入管理员系统                   \n\n");
+    printf ("\t                   3.退出系统                       \n");
     printf ("\t------------------------------------------------------\n");
-    printf ("\t请输入(1/2)：\n");
+    printf ("\t请输入(1/2/3)：\n");
 } 
 
-void Welcome2(){
+void Welcome2(){    //离开界面 
 	printf ("\t*******************当前停车场停放情况*******************\n");
     printf ("\t\t当前停车场共有%d/%d辆车,等候区共有%d/%d辆车\n",p.top+1,PMAX,(s.rear +SMAX - s.front) % SMAX, SMAX-1);
     printf ("\t********************************************************\n\n");
@@ -347,7 +348,7 @@ void Welcome2(){
     printf ("\t------------------------------------------------------\n");
     printf ("\t请输入(1/2/3)：\n");
 }
-void Welcome3(){
+void Welcome3(){   //管理员界面 
 	printf ("\t*******************当前停车场停放情况*******************\n");
     printf ("\t\t当前停车场共有%d/%d辆车,等候区共有%d/%d辆车\n",p.top+1,PMAX,(s.rear +SMAX - s.front) % SMAX, SMAX-1);
     printf ("\t********************************************************\n\n");
@@ -356,14 +357,14 @@ void Welcome3(){
     printf ("\t                   1.停车场停车信息查询               \n");
     printf ("\t                   2.便道停车信息查询               \n");
     printf ("\t                   3.删除信息                     \n");
-    printf ("\t                   4.修改信息                           \n");
-    printf ("\t                   5.退出系统                       \n");
+    printf ("\t                   4.退出系统                       \n");
+    printf ("\t                   5.返回上一级                       \n");
     printf ("\t\n\n");
     printf ("\t------------------------------------------------------\n");
     printf ("\t请输入(1/2/3/4/5)：\n");
 }
 
-void Welcome(){
+void Welcome(){   //用户界面 
 
 	printf ("\t*******************当前停车场停放情况*******************\n");
     printf ("\t\t当前停车场共有%d/%d辆车,等候区共有%d/%d辆车\n",p.top+1,PMAX,(s.rear +SMAX - s.front) % SMAX, SMAX-1);
@@ -373,7 +374,8 @@ void Welcome(){
     printf ("\t                   1.进入停车场                           \n");
     printf ("\t                   2.离开                           \n");
     printf ("\t                   3.查询车辆                       \n");
-    printf ("\t                   4.退出系统                     \n\n\n");
+    printf ("\t                   4.退出系统                     \n");
+    printf ("\t                   5.返回上一级                       \n\n\n");
     printf ("\t收费标准：本停车场按照0.03元/分钟计费            \n\n");
     printf ("\t------------------------------------------------------\n");
     printf ("\t请输入(1/2/3/4)：\n");
@@ -389,10 +391,11 @@ int main(){
 	w.length = 0;
 	w.front = 0;
 	w.rear = 0;
-	Welcome1();
-	int n;
-	scanf("%d",&n);
-	if(n==1){
+	int n,cho;
+	while(1){
+		Welcome1();
+		scanf("%d",&n);
+		if(n==1){    //用户 
 		while(1){
 			Welcome();	
 			int i, cho;
@@ -404,6 +407,7 @@ int main(){
 							printf("\n欢迎下次使用，再见！\n\n");
 							break;
 						} 
+			if(5==i) break;
 			printf("\n返回请输入0\n");
 			begin:	
 				scanf("%d", &cho);
@@ -415,19 +419,22 @@ int main(){
 				goto begin;	
 			}
 		}
-	}else if(n==2){
+	}
+	if(n==2){
 		while(1){
 			Welcome3();	
-			int i, cho;
+			int i;
 			scanf("%d", &i);
 			if(1 == i)  DisPlay();
 			if(2 == i)  DisPlayPave();
 			if(3 == i)  Delete();
-			if(4 == i)  Change();
-			if(5 == i)  {
+			if(4 == i)  {
 							printf("\n欢迎下次使用，再见！\n\n");
-							break;
+							return 0;
 						} 
+			if(5 == i)  {
+							break;
+						} 	
 			printf("\n返回请输入0\n");
 			ok:	
 				scanf("%d", &cho);
@@ -441,6 +448,24 @@ int main(){
 		}
 		
 	}
+	if(n==3){
+		return 0;
+	} 
+	if(n!=1&&n!=2&&n!=3){
+		printf("\n您的输入有误，请输入0返回\n");
+			good:	
+				scanf("%d", &cho);
+			if(0 == cho){
+				continue;
+			}
+			else{
+				printf("您的输入有误，请重新输入\n");
+				goto good;	
+			}
+	}
 	
+	
+	
+	}
 	return 0;
 }
